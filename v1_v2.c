d31e79e77f4940f315c60bbf8897dbad9400df07
Kosmas-Louis Meta <74689374+lkmeta@users.noreply.github.com>
2020-11-23 02:08:09 +0200

    Add files via upload
    Running V1 and V2 parts from Exercise 1.

---
+++ v1_v2.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 2000
+#define BILLION 1000000000L;
+
+struct timespec start_time;
+struct timespec stop_time;
+
+double calculateExecutionTime()
+{
+
+    clock_gettime(CLOCK_MONOTONIC, &stop_time);
+
+    double dSeconds = (stop_time.tv_sec - start_time.tv_sec);
+
+    double dNanoSeconds = (double)(stop_time.tv_nsec - start_time.tv_nsec) / BILLION;
+
+    return dSeconds + dNanoSeconds;
+}
+
+void v1(int *adj, int *c3)
+{
+
+    int i, j, k;
+    printf("\nV1 task...");
+
+    clock_gettime(CLOCK_MONOTONIC, &start_time);
+
+    // look for the triangles
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            for (int k = 0; k < N; k++)
+            {
+
+                if (*(adj + i * N + j) == 1 && *(adj + j * N + k) == 1 && *(adj + k * N + i) == 1)
+                {
+                    *(c3 + i) += 1;
+                    *(c3 + j) += 1;
+                    *(c3 + k) += 1;
+                }
+                else
+                    continue;
+            }
+
+    printf("\nc3 = ");
+    for (i = 0; i < N; i++)
+        printf("%d ", *(c3 + i));
+
+    for (int i = 0; i < N; i++)
+        *(c3 + i) = *(c3 + i) / 6;
+
+    printf("\nfinal c3 = ");
+    for (i = 0; i < N; i++)
+        printf("%d ", *(c3 + i));
+
+    printf("\nV1 run time: %f", calculateExecutionTime());
+}
+
+void v2(int *adj, int *c3)
+{
+    int i, j, k;
+    printf("\nV2 task...");
+
+    clock_gettime(CLOCK_MONOTONIC, &start_time);
+
+    // look for the triangles
+    for (int i = 0; i < N; i++)
+        for (int j = i; j < N; j++)
+            for (int k = j; k < N; k++)
+            {
+
+                if (*(adj + i * N + j) == 1 && *(adj + j * N + k) == 1 && *(adj + k * N + i) == 1)
+                {
+                    *(c3 + i) += 1;
+                    *(c3 + j) += 1;
+                    *(c3 + k) += 1;
+                }
+                else
+                    continue;
+            }
+
+
+    printf("\nc3 = ");
+    for (i = 0; i < N; i++)
+        printf("%d ", *(c3 + i));
+
+    printf("\nV2 run time: %f", calculateExecutionTime());
+}
+
+int main()
+{
+
+    srand(time(NULL));
+    int *adj = (int *)malloc(N * N * sizeof(int));
+    int i, j, k;
+
+    int *c3 = (int *)malloc(N * sizeof(int));
+    for (i = 0; i < N; i++)
+        *(c3 + i) = 0;
+
+    for (i = 0; i < N; i++)
+        for (j = 0; j < N; j++)
+        {
+
+            if (i == j)
+                *(adj + i * N + j) = *(adj + j * N + i) = 0;
+            else
+                *(adj + i * N + j) = *(adj + j * N + i) = rand() % 2;
+        }
+    
+    /*
+    // print the adj matrix 
+    printf("The adj matrix elements are:\n");
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            printf("%d ", *(adj + i * N + j));
+        }
+        printf("\n");
+    }
+
+    */
+
+    /* run v1 task... */
+    v1(adj, c3);
+
+    for (i = 0; i < N; i++)
+        *(c3 + i) = 0;
+
+    /* run v2 task... */
+    v2(adj, c3);
+    
+
+    free(adj);
+    free(c3);
+    return 0;
+}
