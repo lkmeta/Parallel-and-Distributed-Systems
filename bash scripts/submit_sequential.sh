eca32beb76115319137dd061a08cfe761f0fb088
Kosmas-Louis Meta <74689374+lkmeta@users.noreply.github.com>
2020-12-06 01:45:46 +0200

    Update submit_sequential.sh

--- bash scripts/submit_sequential.sh
+++ bash scripts/submit_sequential.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#SBATCH --time=00:20:00
+#SBATCH --time=00:10:00
 #SBATCH --partition=batch
-#SBATCH --cpus-per-task=20  
-cd final
+#SBATCH --cpus-per-task=2 
+
+cd Desktop
 
 make clean
 
@@ -10,37 +11,25 @@ module rm gcc
 module rm OpenCilk/9.0.1
 
 module load gcc
-make all
+make v3 
+make v4
 
-module load OpenCilk/9.0.1
-make cilk
+mkdir results
 
 echo "v3 results" > ./results/v3.txt
 	
-echo "Number of workers: 2 4 5 10 15 20 40" >> ./results/v3.txt
 for file in ./matrices/*; do
 	printf "\n"
 	echo "Running v3 for matrix: "$file >> ./results/v3.txt
-	for i in 2 4 5 10 15 20 40; do
-		./v3 $file $i 1 >> ./results/v3.txt
-	done
-	echo "" >> ./results/v3.txt
+	./v3 $file >> ./results/v3.txt
 	echo "" >> ./results/v3.txt
 done
-
 
 echo "v4 results" > ./results/v4.txt
 	
-echo "Number of workers: 2 4 5 10 15 20 40" >> ./results/v4.txt
 for file in ./matrices/*; do
 	printf "\n"
 	echo "Running v4 for matrix: "$file >> ./results/v4.txt
-	for i in 2 4 5 10 15 20 40; do
-		./v4 $file $i 1 >> ./results/v4.txt
-	done
-	echo "" >> ./results/v4.txt
+    ./v4 $file >> ./results/v4.txt
 	echo "" >> ./results/v4.txt
 done
-
-
-cd ..
