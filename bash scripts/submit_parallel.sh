bbf76542236b16d3c3f2b52c292ba89a80429767
Kosmas-Louis Meta <74689374+lkmeta@users.noreply.github.com>
2020-12-06 02:46:59 +0200

    Update submit_parallel.sh

--- bash scripts/submit_parallel.sh
+++ bash scripts/submit_parallel.sh
@@ -19,12 +19,12 @@ mkdir results
 
 echo "v3_cilk results" > ./results/v3_cilk.txt
 	
-echo "Number of workers: 2 4 5 10 15 20 40" >> ./results/v3_cilk.txt
+echo "Number of workers: 2 4 5 10 15 20" >> ./results/v3_cilk.txt
 echo "" >> ./results/v3_cilk.txt
 for file in ./matrices/*; do
 	printf "\n"
 	echo "Running v3_cilk for matrix: "$file >> ./results/v3_cilk.txt
-	for i in 2 4 5 10 15 20 40; do
+	for i in 2 4 5 10 15 20; do
 		CILK_NWORKERS=$i
 		./v3_cilk $file $i 1 >> ./results/v3_cilk.txt	
 	done
@@ -36,11 +36,11 @@ done
 
 echo "v3_openMP results" > ./results/v3_openMP.txt
 	
-echo "Number of threads: 2 4 5 10 15 20 40" >> ./results/v3_openMP.txt
+echo "Number of threads: 2 4 5 10 15 20" >> ./results/v3_openMP.txt
 for file in ./matrices/*; do
 	printf "\n"
 	echo "Running v3_openMP for matrix: "$file >> ./results/v3_openMP.txt
-	for i in 2 4 5 10 15 20 40; do
+	for i in 2 4 5 10 15 20; do
 		./v3_openMP $file $i 1 >> ./results/v3_openMP.txt
 	done
 	echo "" >> ./results/v3_openMP.txt
@@ -50,11 +50,11 @@ done
 
 echo "v4_cilk results" > ./results/v4_cilk.txt
 	
-echo "Number of workers: 2 4 5 10 15 20 40" >> ./results/v4_cilk.txt
+echo "Number of workers: 2 4 5 10 15 20" >> ./results/v4_cilk.txt
 for file in ./matrices/*; do
 	printf "\n"
 	echo "Running v4_cilk for matrix: "$file >> ./results/v4_cilk.txt
-	for i in 2 4 5 10 15 20 40; do
+	for i in 2 4 5 10 15 20; do
 		CILK_NWORKERS=$i
 		./v4_cilk $file $i 1 >> ./results/v4_cilk.txt
 	done
@@ -66,11 +66,11 @@ done
 
 echo "v4_openMP results" > ./results/v4_openMP.txt
 	
-echo "Number of threads: 2 4 5 10 15 20 40" >> ./results/v4_openMP.txt
+echo "Number of threads: 2 4 5 10 15 20" >> ./results/v4_openMP.txt
 for file in ./matrices/*; do
 	printf "\n"
 	echo "Running v4_openMP for matrix: "$file >> ./results/v4_openMP.txt
-	for i in 2 4 5 10 15 20 40; do
+	for i in 2 4 5 10 15 20; do
 		./v4_openMP $file $i 1 >> ./results/v4_openMP.txt
 	done
 	echo "" >> ./results/v4_openMP.txt
